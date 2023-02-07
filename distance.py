90786ba92700b1d0c17e8e29b8615e4412dc3ce9
longddaays <84114366+longddaays@users.noreply.github.com>
2023-02-07 08:16:16 +0700

    update model

--- distance.py
+++ distance.py
@@ -1,5 +1,5 @@
 import googlemaps
-gmaps = googlemaps.Client(key='AIzaSyD6HqlWEEDsiS41-aoBRdZmUunsMYL-VkI')
+gmaps = googlemaps.Client(key='AIzaSyB5lmWIJS5mYWqzlbTZs1UVeh4zsRjLf-M')
 
 print("vi tri :")
 source = input()
@@ -12,3 +12,20 @@ print("khoang cach la : "+ distance)
 print("thoi gian : "+ time)
 
 
+# import googlemaps
+# gmaps = googlemaps.Client(key='AIzaSyB5lmWIJS5mYWqzlbTZs1UVeh4zsRjLf-M')
+
+# def calDist(data):
+#     dis_matrix = []
+#     time_matrix = []
+#     userloc = input()
+#     for i in range(data.shape[0]):
+#     # for i in range(n):
+#         # data = pd.read_csv('data/dss_data_test.csv')
+#         my_dist = gmaps.distance_matrix(userloc,data['location'].iloc[i])['rows'][0]['elements'][0]
+#         distance = my_dist['distance']['text']
+#         time = my_dist['duration']['text']
+#         dis_matrix.append(distance)
+#         time_matrix.append(time)
+        
+#     return dis_matrix, time_matrix    
