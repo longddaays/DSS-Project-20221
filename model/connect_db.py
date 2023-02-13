24704aafaae2aab510a37d28f4e7d8b77f8390b2
longddaays <84114366+longddaays@users.noreply.github.com>
2023-02-13 13:06:53 +0700

    recomsys user history + time filter + topsis

--- model/connect_db.py
+++ model/connect_db.py
@@ -6,19 +6,17 @@ import pymysql
 import pandas as pd
 import mysql.connector
 
-dbName = "data"
+dbName = "test"
 
 #example : tableName = "dss_data_prep"
 def read_frame_from_db(tableName):
-    sqlEngine       = create_engine('mysql+pymysql://root:@127.0.0.1', pool_recycle=3306)
+    sqlEngine       = create_engine('mysql+pymysql://root:@127.0.0.1', pool_recycle=3600)
     dbConnection    = sqlEngine.connect()
     frame           = pd.read_sql("select * from " + dbName + '.' + tableName, dbConnection);
     pd.set_option('display.expand_frame_repr', False)
     print(frame)
     dbConnection.close()
     return frame
-
-    
 
 #example : csvName = "data/dss_data_prep.csv", tableName = "dss_data_prep"
 def csvToDb(csvName, tableName):
@@ -30,14 +28,13 @@ def csvToDb(csvName, tableName):
     )
     mycursor = mydb.cursor()
     mycursor.execute("drop table if exists " + tableName)
-    
-    df = pd.read_csv(csvName) 
+
+    df = pd.read_csv(csvName)
     dataFrame = pd.DataFrame(data=df)           
     sqlEngine = create_engine('mysql+pymysql://root:@127.0.0.1/' + dbName, pool_recycle=3600)
     dbConnection = sqlEngine.connect()    
     frame = dataFrame.to_sql(tableName, dbConnection, if_exists='fail')
     dbConnection.close()
-csvToDb("./data/dss_data_prep.csv", "dss_data_prep")
-data = read_frame_from_db("dss_data_prep")
-# read_frame_from_db("dss_data_prep")
 
+# csvToDb("data/dss_data_prep.csv", "dss_data_prep")
+# read_frame_from_db("dss_data_prep")
